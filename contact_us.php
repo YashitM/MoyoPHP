291c5c73f37a0003c4c5b8435c559a5ece2db513
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2018-02-08 05:25:46 +0530

    Initial Commit

---
+++ contact_us.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yashi
+ * Date: 08-Feb-18
+ * Time: 5:22 AM
+ */
---
+++ static/js/loader.js
@@ -0,0 +1,11 @@
+$(window).on('load', function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");
+});
+
+
+window.sr = ScrollReveal({ duration: 2000 });
+sr.reveal(document.getElementById('about-left'));
+sr.reveal(document.getElementById('about-right'));
+sr.reveal(document.querySelectorAll('.sub-heading-div'));
+
