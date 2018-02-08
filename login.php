49e7e8e645f457b4c4e82f6eb0ce0d6e723f390e
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2018-02-08 06:24:28 +0530

    Login Page Ported

--- login.php
+++ login.php
@@ -76,6 +76,18 @@
     </div>
 </nav>
 
+
+<div class="container" style="padding-bottom: 30%;">
+    <div class="heading-text">
+        Login
+    </div>
+    <div class="social-buttons">
+        <a title="Facebook" class="social-button facebook" href=""><i class="fa fa-facebook"></i><br></a>
+        <a title="Google" class="social-button google" href=""><i class="fa fa-google-plus"></i><br></a>
+    </div>
+    <br>
+</div>
+
 <footer id="myFooter" class="footer">
     <div class="container">
         <div class="row">
