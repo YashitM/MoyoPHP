5cf5dbafa5ceeaac3d1d1dfa3d897c5ec2816b47
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2018-03-04 23:34:29 +0400

    Test Commit

--- loginG.php
+++ loginG.php
@@ -3,7 +3,8 @@
 	include_once 'libs/SocialAuth/contrib/Google_Oauth2Service.php';
 	
 	// Edit Following 3 Lines
-
+    $clientId = '659267976289-pfhtpi394m8lp0pqredrpp5l0es5sddt.apps.googleusercontent.com'; //Application client ID
+    $clientSecret = 'fcTj0WOsiqSb2-wdSvuAED4J'; //Application client secret
     $redirectURL = 'http://localhost/MoyoPHP/'; //Application Callback URL
 	
 	$gClient = new Google_Client();
