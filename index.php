9e9aaa5c416c2f80fca16052837e38312f6ab3ee
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2018-04-09 01:13:21 +0530

    Added Taken Rides Link to NavBar

--- index.php
+++ index.php
@@ -93,7 +93,10 @@
                         <a class="nav-link" href="search_ride.php">Take Rides</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="view_requests.php">Ride Requests</a>
+                        <a class="nav-link" href="view_requests.php">Requests</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="view_taken_rides.php">Rides Requested</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="view_profile.php">Profile</a>
